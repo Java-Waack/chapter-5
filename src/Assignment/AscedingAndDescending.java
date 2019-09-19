5aee24edbd0857e97473216ace33757f6a6bb6e6
GageWaack <45102148+GageWaack@users.noreply.github.com>
2019-09-19 08:41:39 -0500

    sfa

--- src/Assignment/AscedingAndDescending.java
+++ src/Assignment/AscedingAndDescending.java
@@ -23,7 +23,7 @@ public class AscedingAndDescending {
         {
             if (entry2 > entry3)
             {
-                result = "Ascendng order" + entry1 + " " + entry2 + " " + entry3;
+                result = "Ascending order" + entry1 + " " + entry2 + " " + entry3;
                 result1 = "Descending order " + entry3 + " " + entry2 + " " + entry1;
             }
             else
@@ -31,29 +31,36 @@ public class AscedingAndDescending {
                 result = "Ascending order " + entry1 + " " + entry3 + " " + entry2;
                 result1 = "Descending order " + entry3 + " " + entry1 + " " + entry2;
             }
+            System.out.println("The result is " + result + "\n" + result1);
         }
-        else if (entry2 > entry1 && entry3 > entry2) {
+        else if (entry2 > entry1 && entry3 > entry1)
+        {
             if (entry3 > entry2) {
-                result = "Ascending order" + entry3 + " " + entry2 + " " + entry1;
-                result1 = "Descending order" + entry1 + " " + entry2 + " " + entry3;
+                result = "Ascending order " + entry3 + " " + entry2 + " " + entry1;
+                result1 = "Descending order " + entry1 + " " + entry2 + " " + entry3;
             }
             else {
                 result = "Ascending order " + entry3 + " " + entry1 + " " + entry2;
-                result1 = "Descending order" + entry2 + " " + entry1 + " " + entry3;
+                result1 = "Descending order " + entry2 + " " + entry1 + " " + entry3;
             }
-
+            System.out.println("The result is" + result + "\n" + result1);
             }
 
         else if (entry2 > entry3 && entry3 > entry1) {
             if (entry1 < entry2) {
                 result = "Ascending order " + entry2 + " " + entry3 + " " + entry1;
-                result1 = "Descending order" + entry1 + " " + entry3 + " " + entry2;
+                result1 = "Descending order " + entry1 + " " + entry3 + " " + entry2;
             }
-            else {
-                result = "Ascending order " +
+            else
+                {
+                result = "Ascending order " +entry1 + " " +entry2 + " " +entry3;
+                result1 = "Descending order "+ entry3 + " "+ entry2 + " "+ entry1;
             }
+            System.out.println("The result is " + result + "\n" + result1);
         }
-        System.out.println(result + "\n" + result1);
+
+
+
 
     }
 }
